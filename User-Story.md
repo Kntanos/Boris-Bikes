6507ac74282ee3f39983b068e3d9b787380e729b
Kostas Ntanos <kostas@Kostass-MacBook-Air.local>
2021-09-21 16:57:12 +0100

    Finished 1-7 steps

---
+++ User-Story.md
@@ -0,0 +1,6 @@
+| Objects         | Messages| 
+| --------        | -------- |
+| User            | use_bike     |
+|                 | is_bike_working |
+| Bike            | working |
+| Docking Station | release_bike| 
---
+++ spec/boris_bikes_spec.rb
@@ -0,0 +1,7 @@
+require "docking_station"
+
+describe "DockingStation" do
+  #it "should initialize an object that is an instance of DockingStation" do
+  #  expect(DockingStation.new).to be_a(DockingStation)
+  #end
+end
