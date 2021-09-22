5681c1b49bb932a332f0408f18bdba11b7d23d9f
Tania Rosa Machado Santana <taniasantana@Tanias-MacBook-Air.local>
2021-09-22 15:12:32 +0100

    Step 8

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,7 +1,7 @@
 require "docking_station"
 
 describe "DockingStation" do
-  #it "should initialize an object that is an instance of DockingStation" do
-  #  expect(DockingStation.new).to be_a(DockingStation)
-  #end
+  it 'should respond to release_bike' do
+      expect(DockingStation.new).to respond_to(:release_bike)
+  end
 end
