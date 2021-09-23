da70116639bde8e494dffa8121843b1ea6489d08
Kostas Ntanos <kostasntanos@hotmail.com>
2021-09-23 14:08:08 +0100

    added formatting .rspec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,19 @@
-require "docking_station"
+require 'docking_station'
+require 'bike'
 
-describe "DockingStation" do
+describe DockingStation do
   it 'should respond to release_bike' do
-      expect(DockingStation.new).to respond_to(:release_bike)
+      expect(subject).to respond_to(:release_bike)
+  end
+
+  it 'expects bike to be working' do
+    expect(subject.release_bike.working?). to eq(true) 
+  end
+end
+
+describe "#release_bike" do
+station = DockingStation.new
+  it 'should gets the bike' do
+    expect(station.release_bike).to be_a(Bike)
   end
 end
