0d697b0fb6fe697566cb20bd997950f589062a27
Tania Rosa Machado Santana <taniasantana@Tanias-MacBook-Air.local>
2021-09-23 15:28:21 +0100

    created a unit test to check the dock method accept one arg, and implemented it

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,12 @@
 require 'docking_station'
-require 'bike'
 
 describe DockingStation do
   it 'should respond to release_bike' do
       expect(subject).to respond_to(:release_bike)
   end
-
+  it 'should respond to method dock' do
+      expect(subject).to respond_to(:dock)
+  end
   it 'expects bike to be working' do
     expect(subject.release_bike.working?). to eq(true) 
   end
@@ -15,5 +16,13 @@ describe "#release_bike" do
 station = DockingStation.new
   it 'should gets the bike' do
     expect(station.release_bike).to be_a(Bike)
+  end
+end
+
+describe "#dock" do
+  station = DockingStation.new
+  it 'should accept one argument' do
+    expect(station).to respond_to(:dock).with(1).argument
+    #expect(my_callable).to have_received(:call).with({})
   end
 end
